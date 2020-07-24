e87e453f19681dfeac597275d334a72c79935369
Yash Velagapudi <yashv6655@gmail.com>
2020-07-23 19:12:02 -0700

    Removed some env variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 
 const firebaseApp = firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
+  authDomain: "clone-a3e08.firebaseapp.com",
   databaseURL: process.env.DATABASE_URL,
   projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
+  storageBucket: "clone-a3e08.appspot.com",
   messagingSenderId: process.env.MESSAGE_SENDER_ID,
   appId: process.env.APP_ID,
   measurementId: process.env.MEASUREMENT_ID,
